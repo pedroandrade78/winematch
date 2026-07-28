f5f7f6ef64d272e487760dcbae620b5d8574520e
carla leao <csleao@gmail.com>
2026-07-28 17:48:23 +0100

    ui update

--- ui.py
+++ ui.py
@@ -9,7 +9,7 @@ import os
 import requests
 import streamlit as st
 
-API_URL = os.environ.get("API_URL", "https://winematch-v2-485046883553.europe-west1.run.app")
+API_URL = https://winematch-v2-485046883553.europe-west1.run.app streamlit run ui.py
 
 
 def call_recommend_api(query: str, top_k: int = 5, filters: dict = None):
